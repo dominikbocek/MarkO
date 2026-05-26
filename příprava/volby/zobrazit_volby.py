7963a370f31594ab639489a81ae63b4f6c0d8abd
Dominik Boček <havran.svitavy@gmail.com>
2026-05-27 00:38:27 +0200

    Refactor to list files in 'sada' directory
    Replaced zipfile usage with os.listdir to list files in the 'sada' directory and removed '.DS_Store' from the output.

--- příprava/volby/zobrazit_volby.py
+++ příprava/volby/zobrazit_volby.py
@@ -1,10 +1,9 @@
-import zipfile
-import json
 import os
+import json
 
 os.chdir("../../")
-with zipfile.ZipFile("sada.zip", 'r') as zf: # případné opravení do utf-8: zipu -f
-    seznam = zf.namelist()
-    dirs = list(set([os.path.dirname(x) for x in seznam]))
-    dirs = sorted(dirs)
-    print(json.dumps(dirs))
+seznam = os.listdir("sada")
+dirs = sorted(seznam)
+if ".DS_Store" in dirs:
+    dirs.remove(".DS_Store")
+print(json.dumps(dirs))
