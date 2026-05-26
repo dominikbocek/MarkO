52018320e4f330fcaaaf630454c7b437f8d3cb93
Dominik Boček <havran.svitavy@gmail.com>
2026-05-27 00:36:31 +0200

    Refactor election extraction logic and add symlink creation

--- příprava/volby/extrahovat_volby.py
+++ příprava/volby/extrahovat_volby.py
@@ -2,8 +2,8 @@ import os
 import sys
 import array
 import shutil
-import zipfile
 import argparse
+import subprocess
 from pathlib import Path
 
 sys.stdout.reconfigure(encoding='utf-8')
@@ -15,45 +15,46 @@ volby = argumenty.volby
 
 os.chdir("../../")
 
-
-zf = zipfile.ZipFile("sada.zip", 'r')
+cwd = os.getcwd()
 
 druh_voleb = ""
 
-if volby + "/pscoco.csv" in zf.namelist():
+if "pscoco.csv" in os.listdir(f"sada/{volby}"):
     druh_voleb = "sněmovna"
-elif volby + "/pecoco.csv" in zf.namelist():
+elif "pecoco.csv" in os.listdir(f"sada/{volby}"):
     druh_voleb = "prezident"
-elif volby + "/kzcoco.csv" in zf.namelist():
+elif "kzcoco.csv" in os.listdir(f"sada/{volby}"):
     druh_voleb = "kraje"
 else:
     druh_voleb = "žádné"
     sys.exit("druhvoleb = '" + druh_voleb + "'")
 
-with zipfile.ZipFile("okrsky.zip", "r") as zf2:
-    if druh_voleb != "prezident":
-        if not os.path.exists("public/volby/" + volby + "/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
-            zf2.extractall("public/volby/" + volby)
-    else:
-        if not os.path.exists("public/volby/" + volby + "/první kolo/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
-            zf2.extractall("public/volby/" + volby + "/první kolo")
-            zf2.extractall("public/volby/" + volby + "/druhé kolo")
 
-for f in zf.namelist():
-    if f.startswith(volby + "/") and f != str(volby + "/"):
-        if druh_voleb == "prezident":
-            if not os.path.exists("public/volby/" + volby + "/první kolo/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
-                zf.extract(f, "public/volby/" + volby + "/první kolo")
-                zf.extract(f, "public/volby/" + volby + "/druhé kolo")
-                shutil.move("public/volby/" + volby + "/první kolo/" + f, "public/volby/" + volby + "/první kolo/" + f.split("/")[1])
-                shutil.move("public/volby/" + volby + "/druhé kolo/" + f, "public/volby/" + volby + "/druhé kolo/" + f.split("/")[1])
-        else:
-            if not os.path.exists("public/volby/" + volby + "/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
-                zf.extract(f, "public/volby/" + volby)
-                shutil.move("public/volby/" + volby + "/" + f, "public/volby/" + volby + "/" + f.split("/")[1])
+# symbolické odkazy pro program a volební data
 
-with zipfile.ZipFile(druh_voleb + ".zip") as zf3:
-    if not os.path.exists("public/volby/" + volby + "/volebni_mapy.sh") and not os.path.exists("public/volby/" + volby + "/první kolo/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
-        zf3.extractall("public/volby/" + volby)
+os.mkdir(f"public/volby/{volby}")
+
+if druh_voleb == "prezident":
+    os.mkdir(f"public/volby/{volby}/první kolo")
+    os.mkdir(f"public/volby/{volby}/druhé kolo")
+    if not os.path.exists(f"public/volby/{volby}/volebni_mapy.sh") and not os.path.exists(f"public/volby/{volby}/první kolo/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
+        for soubor in os.listdir(f"{druh_voleb}/první kolo"): # kopírování programu
+            subprocess.run(["ln", f"{cwd}/{druh_voleb}/první kolo/{soubor}", f"{cwd}/public/volby/{volby}/první kolo/{soubor}"])
+        for soubor in os.listdir(f"{druh_voleb}/druhé kolo"): # kopírování programu
+            subprocess.run(["ln", f"{cwd}/{druh_voleb}/druhé kolo/{soubor}", f"{cwd}/public/volby/{volby}/druhé kolo/{soubor}"])
+        for soubor in os.listdir("okrsky"): # kopírování okrsků
+            subprocess.run(["ln", f"{cwd}/okrsky/{soubor}", f"{cwd}/public/volby/{volby}/první kolo/{soubor}"])
+            subprocess.run(["ln", f"{cwd}/okrsky/{soubor}", f"{cwd}/public/volby/{volby}/druhé kolo/{soubor}"])
+        for soubor in os.listdir(f"sada/{volby}"): # kopírování volebních dat
+            subprocess.run(["ln", f"{cwd}/sada/{volby}/{soubor}", f"{cwd}/public/volby/{volby}/první kolo/{soubor}"])
+            subprocess.run(["ln", f"{cwd}/sada/{volby}/{soubor}", f"{cwd}/public/volby/{volby}/druhé kolo/{soubor}"])
+else:
+    if not os.path.exists(f"public/volby/{volby}/volebni_mapy.sh") and not os.path.exists(f"public/volby/{volby}/první kolo/volebni_mapy.sh"): # ověření, jestli už nedošlo k extrakci
+        for soubor in os.listdir(druh_voleb): # kopírování programu
+            subprocess.run(["ln", f"{cwd}/{druh_voleb}/{soubor}", f"{cwd}/public/volby/{volby}/{soubor}"])
+        for soubor in os.listdir("okrsky"): # kopírování okrsků
+            subprocess.run(["ln", f"{cwd}/okrsky/{soubor}", f"{cwd}/public/volby/{volby}/{soubor}"])
+        for soubor in os.listdir(f"sada/{volby}"): # kopírování volebních dat
+            subprocess.run(["ln", f"{cwd}/sada/{volby}/{soubor}", f"{cwd}/public/volby/{volby}/{soubor}"])
 
 print(druh_voleb)
